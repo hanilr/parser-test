94efe350bb42a1fbdae519d53b867aac9168d54a
hanilr <hakkianilragiboglu@gmail.com>
2021-12-31 17:30:54 +0300

    Missing argument check added

--- src/main.c
+++ src/main.c
@@ -11,8 +11,14 @@
 
 int main(int argc, char *argv[])
 {
+    if(argc < 2)
+    {
+        printf("\nMISSING ARGUMENT!\n");
+        return 1;
+    }
+
     const char *file_content = read_file(argv[1]);
-    if(parser(file_content) != 0) { printf("PARSER ERROR"); }
+    if(parser(file_content) != 0) { printf("\nPARSER ERROR!\n"); }
     
     return 0;
 }
