8c247c2faa9266f5b76af942b3687b196f5347ba
hanilr <hakkianilragiboglu@gmail.com>
2022-01-09 01:05:02 +0300

    Live command section added

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 /* STANDARD LIBRARY */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* DIY LIBRARY */
 #include "lib/file.h"
@@ -11,14 +13,32 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2)
+    if(argc == 1)
+    {
+        printf("\nParser-acc v1.0");
+        char *com = (char*) malloc(128);
+        while(1)
+        {
+            printf("\n> ");
+            scanf("%s", com);
+            if(subinstr(com, "quit()") == 0) { break; }
+            else
+            {
+                FILE *wf = fopen("test/temp", "w");
+                fwrite(com, 1, strlen(com), wf);
+                fclose(wf);
+                parser("test/temp");
+                remove("test/temp");
+            }
+        }
+        free(com);
+    }
+    else if(argc > 2)
     {
         fprintf(stderr, "MISSING ARGUMENT!");
         return 1;
     }
-
     else { if(parser(argv[1]) != 0) { fprintf(stderr, "PARSER ERROR!"); } }
-
     return 0;
 }
 
