350207fc41002d2b81d26162abd998cd54cfcd0c
hanilr <hakkianilragiboglu@gmail.com>
2022-01-11 15:05:41 +0300

    For statements converted to while

--- src/util.c
+++ src/util.c
@@ -12,15 +12,19 @@
 
 int chrepeat(const char *str, char ascii)
 {
-    int word_buffer = 0;
-    for(int i = 0; strlen(str) > i; i+=1) { if(str[i] == ascii) { word_buffer+=1; } }
+    int word_buffer = 0, i = 0;
+    while(strlen(str) > i)
+    {
+        if(str[i] == ascii) { word_buffer+=1; }
+        i+=1;
+    }
     return word_buffer;
 }
 
 int dislen(const char *str, int pos, char *chr1, char *chr2)
 {
     int x = pos, y = pos;
-    for(pos; strlen(str) > pos; pos+=1)
+    while(strlen(str) > pos)
     {
         if(str[x] == chr1[0])
         {
@@ -28,45 +32,54 @@ int dislen(const char *str, int pos, char *chr1, char *chr2)
             else { y+=1; }
         }
         else { x+=1; }
+        pos+=1;
     }
     return y-x-1;
 }
 
 int subinstr(const char *str, char *chr)
 {
-    int word_pos, buffer;
-    for(word_pos = 0, buffer = 0; strlen(str) > word_pos; word_pos+=1)
+    int word_pos = 0, buffer = 0;
+    while(strlen(str) > word_pos)
     {
         if(str[word_pos] == chr[buffer]) { buffer+=1; }
         else { buffer = 0; }
         if(strlen(chr) == buffer) { return 0; }
+        word_pos+=1;
     }
     return 1;
 }
 
 int lastpos(const char *str, char *chr)
 {
-    int word_pos, buffer;
-    for(word_pos = 0, buffer = 0; strlen(str) > word_pos; word_pos+=1)
+    int word_pos = 0, buffer = 0;
+    while(strlen(str) > word_pos)
     {
         if(str[word_pos] == chr[buffer]) { buffer+=1; }
         else { buffer = 0; }
         if(strlen(chr) == buffer) { break; }
+        word_pos+=1;
     }
     return word_pos;
 } /* START WITH 0 */
 
 int lastpos_line(const char *str, char *chr, int line)
 {
-    int pos = lastpos(str, chr), pos_line;
+    int pos = lastpos(str, chr), pos_line, x = 0, y = 0;
     if(line == 1) { return pos; }
     char *temp = (char*) malloc(strlen(str)+1);
 
-    for(int i = 0; line > i; i+=1)
+    while(line > x)
     {
-        for(int i = 0; strlen(str)-pos > i; i+=1) { temp[i] = str[pos+i]; }
+        while(strlen(str)-pos > y)
+        {
+            temp[y] = str[pos+y];
+            y+=1;
+        }
+        y = 0;
         pos_line = lastpos(temp, chr);
         pos += pos_line+1;
+        x+=1;
     }
 
     free(temp);
