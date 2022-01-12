94a1b568011d14eb2bad819241de6ed8da34e671
hanilr <hakkianilragiboglu@gmail.com>
2022-01-12 21:54:15 +0300

    Function 'file_parser' added. Function 'parser' optimized.

--- src/parser.c
+++ src/parser.c
@@ -43,12 +43,12 @@ void calc_parser(char *raw, int arg_count)
     printf("%d ", result);
 }
 
-void parser(char *file_name)
+void file_parser(char *file_name)
 {
     const char *whole_file = read_file(file_name);
     int line_of_str = chrepeat(whole_file, '\n')+1, i = 1;
     if(chrepeat(whole_file, '\n') == 0) { line_of_str = 2; } /* IF ONLY ONE LINE IN A FILE */
-
+    
     struct variable var[127];
     int var_count = 0;
 
@@ -56,149 +56,153 @@ void parser(char *file_name)
     {
         const char *str_temp = (char*) malloc(strlen(get_line(file_name, i))+1);
         str_temp = get_line(file_name, i);
-
-        /* STATEMENTS */
         if(i == line_of_str && whole_file[strlen(whole_file)] == '\0') { break; } /* IF LAST LINE EMPTY */
-        if(subinstr(str_temp, "#") == 0) /* COMMENT SECTION */
-        {
-            char *temp = (char*) malloc(strlen(str_temp)+1);
-            int comment_pos = lastpos(str_temp, "#"), x = 0;
-            memset(temp, 0, strlen(temp)+1);
+        parser((char*) str_temp, var, var_count);
+        i+=1;
+    }
+}
 
-            while(comment_pos-1 > x)
-            {
-                temp[x] = str_temp[x];
-                x+=1;
-            }
-            str_temp = temp;
-            free(temp);
-        }
-        if(str_temp[0] == '$') /* VARIABLE SECTION */
+void parser(char *str_temp, struct variable *var, int var_count)
+{
+    /* STATEMENTS */
+    if(subinstr(str_temp, "#") == 0) /* COMMENT SECTION */
+    {
+        char *temp = (char*) malloc(strlen(str_temp)+1);
+        int comment_pos = lastpos(str_temp, "#"), x = 0;
+        memset(temp, 0, strlen(temp)+1);
+
+        while(comment_pos-1 > x)
         {
-            if(var_count == 128)
+            temp[x] = str_temp[x];
+            x+=1;
+        }
+        str_temp = temp;
+        free(temp);
+    }
+    if(str_temp[0] == '$') /* VARIABLE SECTION */
+    {
+        if(var_count == 128)
+        {
+            fprintf(stderr, "PARSER ERROR: Too much variable!");
+            exit(EXIT_FAILURE);
+        }
+
+        int var_pos = lastpos(str_temp, "$")+1, x = 0, y = 0;
+        int content_pos = lastpos(str_temp, "\"")+1;
+        char *temp = (char*) malloc(strlen(str_temp)+1);
+
+        while(strlen(str_temp) > x)
+        {
+            temp[x] = str_temp[var_pos+x];
+            if(str_temp[var_pos+x+1] == ' ' || str_temp[var_pos+x+1] == '=')
             {
-                fprintf(stderr, "PARSER ERROR: Too much variable!");
+                temp[x+1] = '\0';
+                break;
+            }
+            x+=1;
+        }
+        strcpy(var[var_count].name, temp);
+        memset(temp, 0, strlen(temp)+1);
+        while(strlen(str_temp) > y)
+        {
+            temp[y] = str_temp[content_pos+y];
+            if(str_temp[content_pos+y+1] == '\"')
+            {
+                temp[y+1] = '\0';
+                break;
+            }
+            y+=1;
+        }
+        strcpy(var[var_count].value, temp);
+        free(temp);
+        var_count+=1;
+    }
+
+    /* COMMANDS */
+    if(str_temp[0] == '(' && (subinstr(str_temp, "+") == 0 || subinstr(str_temp, "-") == 0 ||subinstr(str_temp, "*") == 0 ||subinstr(str_temp, "/") == 0))
+    {
+        if(subinstr(str_temp, "(") != 0 || subinstr(str_temp, ")") != 0)
+        {
+            fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (MATH)");
+            exit(EXIT_FAILURE);
+        }
+
+        int content_line = dislen(str_temp, 0, "(", ")");
+        int arg_count = chrepeat(str_temp, ' '), x = 0;
+
+        if(str_temp[strlen(str_temp)] == ' ') { arg_count-=1; }
+
+        char *content = (char*) malloc(content_line+1);
+        while(content_line > x)
+        {
+            content[x] = str_temp[x+1];
+            x+=1;
+        }
+        calc_parser(content, arg_count);
+        free(content);
+    } /* MATH */
+    if(subinstr(str_temp, "print") == 0)
+    {
+        /* ERRORS */
+        if(subinstr(str_temp, "(") != 0 || subinstr(str_temp, ")") != 0)
+        {
+            fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (PRINT)");
+            exit(EXIT_FAILURE);
+        }
+
+        int command_pos = lastpos(str_temp, "print"), x = 0;
+        int content_line = dislen(str_temp, command_pos, "(", ")");
+        char *content = (char*) malloc(content_line+1);
+
+         memset(content, 0, content_line+1);
+        while(content_line > x)
+        {
+            content[x] = str_temp[command_pos+x+2];
+            x+=1;
+        }
+        content[content_line+1] = '\0';
+
+        if(subinstr(content, "$") == 0)
+        {
+            if(subinstr(content, "\"") == 0)
+            {
+                fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (VARIABLE)");
                 exit(EXIT_FAILURE);
             }
 
-            int var_pos = lastpos(str_temp, "$")+1, x = 0, y = 0;
-            int content_pos = lastpos(str_temp, "\"")+1;
-            char *temp = (char*) malloc(strlen(str_temp)+1);
+            char *var_buffer = (char*) malloc(content_line);
+            int var_pos = lastpos(content, "$");
+            int var_len = dislen(content, var_pos, "$", "\0");
+            int var_point, y = 0, z = 0;
 
-            while(strlen(str_temp) > x)
+            while(var_len+1 > y)
             {
-                temp[x] = str_temp[var_pos+x];
-                if(str_temp[var_pos+x+1] == ' ' || str_temp[var_pos+x+1] == '=')
+                var_buffer[y] = content[var_pos+y+1];
+                if(y == var_len)
                 {
-                    temp[x+1] = '\0';
-                    break;
-                }
-                x+=1;
-            }
-            strcpy(var[var_count].name, temp);
-            memset(temp, 0, strlen(temp)+1);
-            while(strlen(str_temp) > y)
-            {
-                temp[y] = str_temp[content_pos+y];
-                if(str_temp[content_pos+y+1] == '\"')
-                {
-                    temp[y+1] = '\0';
+                    var_buffer[y+1] == '\0';
                     break;
                 }
                 y+=1;
             }
-            strcpy(var[var_count].value, temp);
-            free(temp);
-            var_count+=1;
-        }
-
-        /* COMMANDS */
-        if(str_temp[0] == '(' && (subinstr(str_temp, "+") == 0 || subinstr(str_temp, "-") == 0 ||subinstr(str_temp, "*") == 0 ||subinstr(str_temp, "/") == 0))
-        {
-            if(subinstr(str_temp, "(") != 0 || subinstr(str_temp, ")") != 0)
+            while(128 > z)
             {
-                fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (MATH)");
-                exit(EXIT_FAILURE);
-            }
-
-            int content_line = dislen(str_temp, 0, "(", ")");
-            int arg_count = chrepeat(str_temp, ' '), x = 0;
-
-            if(str_temp[strlen(str_temp)] == ' ') { arg_count-=1; }
-
-            char *content = (char*) malloc(content_line+1);
-            while(content_line > x)
-            {
-                content[x] = str_temp[x+1];
-                x+=1;
-            }
-            calc_parser(content, arg_count);
-            free(content);
-        } /* MATH */
-        if(subinstr(str_temp, "print") == 0)
-        {
-            /* ERRORS */
-            if(subinstr(str_temp, "(") != 0 || subinstr(str_temp, ")") != 0)
-            {
-                fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (PRINT)");
-                exit(EXIT_FAILURE);
-            }
-
-            int command_pos = lastpos(str_temp, "print"), x = 0;
-            int content_line = dislen(str_temp, command_pos, "(", ")");
-            char *content = (char*) malloc(content_line+1);
-
-            memset(content, 0, content_line+1);
-            while(content_line > x)
-            {
-                content[x] = str_temp[command_pos+x+2];
-                x+=1;
-            }
-            content[content_line+1] = '\0';
-
-            if(subinstr(content, "$") == 0)
-            {
-                if(subinstr(content, "\"") == 0)
+                if(subinstr(var[z].name, var_buffer) == 0)
                 {
-                    fprintf(stderr, "PARSER ERROR: Uncorrect syntax! (VARIABLE)");
-                    exit(EXIT_FAILURE);
+                    var_point = z;
+                    break;
                 }
-
-                char *var_buffer = (char*) malloc(content_line);
-                int var_pos = lastpos(content, "$");
-                int var_len = dislen(content, var_pos, "$", "\0");
-                int var_point, y = 0, z = 0;
-
-                while(var_len+1 > y)
-                {
-                    var_buffer[y] = content[var_pos+y+1];
-                    if(y == var_len)
-                    {
-                        var_buffer[y+1] == '\0';
-                        break;
-                    }
-                    y+=1;
-                }
-                while(128 > z)
-                {
-                    if(subinstr(var[z].name, var_buffer) == 0)
-                    {
-                        var_point = z;
-                        break;
-                    }
-                    if(z == 127) { exit(EXIT_FAILURE); }
-                    z+=1;
-                }
-                print(var[var_point].value);
-                free(var_buffer);
-            } /* VARIABLE SECTION */
-            else { print(content); }
-            free(content);
-        } /* PRINT */
-        if(subinstr(str_temp, "clrscr()") == 0) { clrscr(); } /* CLEAR SCREEN */
-        free((char*) str_temp);
-        i+=1;
-    }
+                if(z == 127) { exit(EXIT_FAILURE); }
+                z+=1;
+            }
+            print(var[var_point].value);
+            free(var_buffer);
+        } /* VARIABLE SECTION */
+        else { print(content); }
+        free(content);
+    } /* PRINT */
+    if(subinstr(str_temp, "clrscr()") == 0) { clrscr(); } /* CLEAR SCREEN */
+    free((char*) str_temp);
 }
 
 /* MADE BY @hanilr */
